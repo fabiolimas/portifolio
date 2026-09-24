ffdeefc1babdf9105b331ae77baa751caeea7532
Fábio Lima <fabiolima01@live.com>
2026-09-24 09:29:09 -0300

    tagmanager

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -49,7 +49,18 @@
 
   gtag('config', 'G-5NYKF25MHX');
 </script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K2GV8SBX');</script>
+<!-- End Google Tag Manager -->
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K2GV8SBX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <div id="app">
         <nav id="gc-nav" class="gc-navbar navbar navbar-expand-lg fixed-top py-3 gc-scrolled">
       <div class="container">
