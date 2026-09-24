c1df1eb479b477db5317a9b9fb0de42030edc2dd
Fábio Lima <fabiolima01@live.com>
2026-09-24 09:19:07 -0300

    adição googletag

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -40,6 +40,15 @@
     />
     <link rel="stylesheet" href="{{asset('assets/css/front.css')}}">
       </head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5NYKF25MHX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5NYKF25MHX');
+</script>
 <body>
     <div id="app">
         <nav id="gc-nav" class="gc-navbar navbar navbar-expand-lg fixed-top py-3 gc-scrolled">
